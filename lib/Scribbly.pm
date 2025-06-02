fdbf85669761b150f63e5de8043925b49dd0f633
Karlix <bloxyp@gmail.com>
2025-06-02 22:24:56 +0800

    Workspace added

--- lib/Scribbly.pm
+++ lib/Scribbly.pm
@@ -2,7 +2,7 @@ package Scribbly;
 use Dancer2;
 
 get '/' => sub {
-    send_file('index.html', system_path => 0);  # load views/index.html
+    template 'index';
 };
 
-true;  # must return true at the end of any module
+1;
