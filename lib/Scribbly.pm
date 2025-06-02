efed44db84e58e67d5c32b0e322692d7a4d09763
Karlix <bloxyp@gmail.com>
2025-06-02 23:06:51 +0800

    basic implementation of login

--- lib/Scribbly.pm
+++ lib/Scribbly.pm
@@ -1,8 +1,31 @@
-package Scribbly;
 use Dancer2;
 
+set template => 'template_toolkit';
+
+# Show login page
 get '/' => sub {
     template 'index';
 };
 
-1;
+# Handle login submission
+post '/login' => sub {
+    my $username = body_parameters->get('username');
+    my $password = body_parameters->get('password');
+
+    if ($username eq 'admin' && $password eq '1234') {
+        session user => $username;
+        redirect '/workspace';
+    } else {
+        template 'index', { error => 'Invalid credentials' };
+    }
+};
+
+# Show the workspace page
+get '/workspace' => sub {
+    if (!session('user')) {
+        redirect '/';
+    }
+    template 'workspace', { user => session('user') };
+};
+
+start;
